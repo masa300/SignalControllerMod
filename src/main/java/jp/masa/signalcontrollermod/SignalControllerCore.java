5240080606601f80979a9dc19fd41e6bcd295b60
Kaiz <kaiz@kaiz.jp>
2021-05-16 23:04:04 +0900

    1.12.2-1.4.0

--- src/main/java/jp/masa/signalcontrollermod/SignalControllerCore.java
+++ src/main/java/jp/masa/signalcontrollermod/SignalControllerCore.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @Mod(modid = SignalControllerCore.MODID, version = SignalControllerCore.VERSION, name = SignalControllerCore.MODID)
 public class SignalControllerCore {
     public static final String MODID = "signalcontrollermod";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
 
 
     public static Block SIGNAL_CONTROLLER_BLOCK = new SignalController();
