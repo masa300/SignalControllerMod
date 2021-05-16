700b8c64e3429366d5306b888df42a025abe6953
Kaiz <kaiz@kaiz.jp>
2021-05-17 00:18:38 +0900

    パケット受信時の挙動を追加

--- src/main/java/jp/masa/signalcontrollermod/block/tileentity/TileEntityCustom.java
+++ src/main/java/jp/masa/signalcontrollermod/block/tileentity/TileEntityCustom.java
@@ -1,6 +1,19 @@
 package jp.masa.signalcontrollermod.block.tileentity;
 
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityCustom extends TileEntity {
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return this.writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        this.readFromNBT(pkt.getNbtCompound());
+    }
 }
